f6d314e495ca1406ff91ed65aa87312b4bdafd56
TheColdPot <66854652+TheColdPot@users.noreply.github.com>
2020-12-12 16:28:10 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1879,6 +1879,7 @@ var cnames_active = {
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
   "potion": "finnfiddle.github.io/potion",
   "pp": "promptpay.github.io",
+  "pecis": "techpot-studio.github.io/pecis"
   "ppt": "1ocalhost.github.io/ppt",
   "pptx": "meshesha.github.io/jspptxjs.github.io",
   "pranshuchittora": "pranshuchittora.github.io",
